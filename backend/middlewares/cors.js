9abd347ad72ed98a93904edc48082ee9b17ad42a
jul0706 <yu.ivan@rambler.ru>
2023-07-03 17:35:26 +0500

    cookie5

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -5,9 +5,9 @@ function allowCors(req, res, next) {
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const { origin } = req.headers;
+  res.header('Access-Control-Allow-Credentials', 'true');
   if (allowedCorsDomain.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', 'true');
   }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
