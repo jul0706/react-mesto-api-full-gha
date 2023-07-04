6d4024a466ed4d9e1fd15c1f7f6df9c5aa97669f
jul0706 <yu.ivan@rambler.ru>
2023-07-04 17:01:53 +0500

    card like

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,33 +1,33 @@
 import { CurrentUserContext } from '../contexts/CurrentUserContext';
 import React, { useContext } from 'react';
 
-function Card ({cardData, onCardClick, onCardLike, onCardDelete}) {
-    
+function Card({ cardData, onCardClick, onCardLike, onCardDelete }) {
+
     const user = useContext(CurrentUserContext); //подписались на контекст текущего пользователя 
 
-    const isUserOwner = cardData.owner._id === user._id; //является ли пользователь владельцем карточки
-    const isUserLiked = cardData.likes.some((userLiked)=>{ //лайкал ли пользователь карточку
+    const isUserOwner = cardData.owner === user._id; //является ли пользователь владельцем карточки
+    const isUserLiked = cardData.likes.some((userLiked) => { //лайкал ли пользователь карточку
         return userLiked._id === user._id
     })
 
-    function handleImageClick () { //обработчик открытия попапа с изображением
+    function handleImageClick() { //обработчик открытия попапа с изображением
         onCardClick(cardData)
     }
 
-    function handleLikeClick () { //обработчик лайка/дизлайка карточки
+    function handleLikeClick() { //обработчик лайка/дизлайка карточки
         onCardLike(cardData)
     }
 
-    function handleCardDelete () { // обработчик удаления карточки
+    function handleCardDelete() { // обработчик удаления карточки
         onCardDelete(cardData._id)
     }
-    
+
     return (
         <article className="place">
             {isUserOwner && <button onClick={handleCardDelete} className="place__delete-icon"></button>}
-            <img 
-                src={`${cardData ? cardData.link : ''}`} 
-                className="place__image" 
+            <img
+                src={`${cardData ? cardData.link : ''}`}
+                className="place__image"
                 onClick={handleImageClick}
                 alt={cardData.name}
             /> {/*Изобажение*/}
@@ -36,8 +36,8 @@ function Card ({cardData, onCardClick, onCardLike, onCardDelete}) {
                     {cardData.name}
                 </h2>
                 <div className="place__like-area">
-                    <button 
-                        type="button" 
+                    <button
+                        type="button"
                         className={`place__like-button ${isUserLiked && 'place__like-button_active'}`} //если пользоваетль лайкал карточку, добавить класс
                         aria-label="кнопка понравилось"
                         onClick={handleLikeClick}>
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -17,7 +17,7 @@ function Login({ onLogin, setEmail, setisInfoTooltipPopupOpen, setisAuthComplete
         }
         auth.login(formValue) //запрос к API на вход
             .then((userData) => {
-                //localStorage.setItem('token', data.token); //сохранили токен
+                localStorage.setItem('login', true);
                 setCurrentUser(userData)
                 onLogin(true);
                 setEmail(userData.email);
