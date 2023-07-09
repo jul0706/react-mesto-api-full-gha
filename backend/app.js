2180fe103e237b214fdd56c9e82cf9ce39ed4d9f
jul0706 <yu.ivan@rambler.ru>
2023-07-09 15:21:21 +0500

    tests8

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+/* require('dotenv').config(); */
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
